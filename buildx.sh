d5afc9743e83a999fc2260dc34a73356cb579206
mikenye <mike.nye@xpanse.com.au>
2020-03-01 22:39:07 +0800

    Add buildx script

---
+++ buildx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -x
+
+IMAGE=mikenye/flightairmap
+
+# Build the image using buildx
+docker buildx build -t ${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+
+VERSION=$(docker run --rm -it --entrypoint cat mikenye/flightairmap:latest /VERSION | cut -c1-14)
+
+docker buildx build -t ${IMAGE}:${VERSION} --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+
