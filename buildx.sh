e6d0feade8d5e94cdb06918cc9e466e31e4fb723
mikenye <mike.nye@xpanse.com.au>
2020-03-17 21:21:28 +0800

    Fix build script.

--- buildx.sh
+++ buildx.sh
@@ -6,8 +6,8 @@ IMAGE=mikenye/flightairmap
 
 # Build the image using buildx
 docker buildx build -t ${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
+docker pull ${IMAGE}:latest
 
-VERSION=$(docker run --rm -it --entrypoint cat mikenye/flightairmap:latest /VERSION | cut -c1-14)
+VERSION=$(docker run --rm --entrypoint cat mikenye/flightairmap:latest /VERSION | cut -c1-14)
 
 docker buildx build -t ${IMAGE}:${VERSION} --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
-
