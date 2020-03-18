c7ffa4c5f125ef65dfa4abdf8e55de201eb84139
mikenye <mike.nye@xpanse.com.au>
2020-03-18 18:31:14 +0800

    Add context use and buildx use

--- buildx.sh
+++ buildx.sh
@@ -4,6 +4,10 @@ set -x
 
 IMAGE=mikenye/flightairmap
 
+docker context use x86_64
+export DOCKER_CLI_EXPERIMENTAL="enabled"
+docker buildx use homecluster
+
 # Build the image using buildx
 docker buildx build -t ${IMAGE}:latest --compress --push --platform linux/amd64,linux/arm/v7,linux/arm64 .
 docker pull ${IMAGE}:latest
