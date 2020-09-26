23c0fe353f883afcd1961632838f996a25c72ff6
Mike <mike@narx.net>
2020-09-26 14:24:55 +0800

    Fail on error

--- buildx.sh
+++ buildx.sh
@@ -4,6 +4,8 @@ REPO=mikenye
 IMAGE=flightairmap
 PLATFORMS="linux/amd64,linux/arm/v6,linux/arm/v7,linux/arm64"
 
+set -e
+
 docker context use x86_64
 export DOCKER_CLI_EXPERIMENTAL="enabled"
 docker buildx use homecluster
